2652caef7cd450b06e84ffb68b88fd0ea791679a
Rushikesh Ganachari <rushikeshganachari111@gmail.com>
2025-03-03 00:45:44 +0530

    Update app routes for better navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AddExpenseComponent } from './pages/add-expense/add-expense.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default Route
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent }, 
+  { path: 'add-expense', component: AddExpenseComponent }, 
+  { path: 'reports', component: ReportsComponent }, 
+];
